e9898d18a602126f380289f3a0aad56c6953ba15
arnaud <arnaud.cossu@gmail.com>
2022-02-12 14:52:53 +0100

    tp3

--- S2/Programmation MultiTâches/TP3/fournis/client.c
+++ S2/Programmation MultiTâches/TP3/fournis/client.c
@@ -8,105 +8,152 @@
 #include<string.h>
 #include <sys/stat.h>
 
+#define ERROR -1
+
 #define MAX_BUFFER_SIZE 146980
 
-int main(int argc, char *argv[]) {
+/**
+ * Protocole de réception d'un message :
+ *  1 - Envoie de la taille du message ;
+ *  2 - Envoie du message en lui-même.
+ **/
+int sendTCP(int connectedSocket, char *message, size_t messageSize) {
+    ssize_t res = send(connectedSocket, &messageSize, sizeof(messageSize), 0);
 
-  if (argc != 4){
-    printf("utilisation : client ip_serveur port_serveur nom_fichier\n Remaque : le parametre nom_fichier correspond au nom d'un fichier existant dans le répertoire emission\n");
-    exit(0);
-  }
-
-  /* etape 1 : créer une socket */   
-  int ds = socket(PF_INET, SOCK_STREAM, 0);
-  if (ds == -1){
-    perror("[Client] : pb creation socket :");
-    exit(1); // je choisis ici d'arrêter le programme car le reste
-	     // dépendent de la réussite de la création de la socket.
-  }
-  printf("[Client] : creation de la socket réussie \n");
-  
-  /* etape 2 : designer la socket du serveur */
-  struct sockaddr_in srv;
-  srv.sin_family = AF_INET;
-  srv.sin_addr.s_addr = inet_addr(argv[1]); //91.174.102.81:32768
-  srv.sin_port = htons(atoi(argv[2]));
-  socklen_t lgA = sizeof(struct sockaddr_in);
- 
-  /* etape 3 : demander une connexion */
-  printf("Demande de connexion\n");
-  int res = connect(ds, (struct sockaddr *)&srv, lgA);
-  if(res == -1){
-    printf("erreur lors de la demande de connexion\n");
-  }
- 
-  /* etape 4 : envoi de fichier : attention la question est générale. Il faut creuser pour définir un protocole d'échange entre le client et le serveur pour envoyer correctement un fichier */
-  int totalSent = 0; // variable pour compter le nombre total d'octet effectivement envoyés au serveur du début à la fin des échanges.
-
-
- 
-  /* le bout de code suivant est une lecture de contenu d'un fichier dont le nom est passé en paramètre.
-     - pour lire un fichier, il faut l'ouvrir en mode lecture
-     - la lecture se fait par blocs d'octets jusqu'à la fin du fichier.
-     - la taille d'un bloc est définie par la constante MAX_BUFFER_SIZE que vous pouvez modifier.
-
-     Le code est à compléter pour mettre en place l'envoi d'un fichier.
-  */
-
-  // construction du nom du chemin vers le fichier
-  char* filepath = malloc(strlen(argv[3]) + 16); // ./emission/+nom fichier +\0
-  filepath[0] = '\0';
-  strcat(filepath, "./emission/");
-  strcat(filepath, argv[3]);
-
-  printf("Client: je vais envoyer %s\n", filepath);
-
-  // obtenir la taille du fichier
-  struct stat attributes;
-  if(stat(filepath, &attributes) == -1){
-    perror("Client : erreur stat");
-    free(filepath);
-    exit(1);
-  }
-
-  int file_size = attributes.st_size; // cette copie est uniquement informer d'où obtenir la taille du fichier.
-  
-  printf("Client : taille du fichier : %d octets\n", file_size);
-  
-  // lecture du contenu d'un fichier
-  FILE* file = fopen(filepath, "rb");
-  if(file == NULL){
-    perror("Client : erreur ouverture fichier \n");
-    free(filepath);
-    exit(1);   
-  }
-  free(filepath);
-
-  int total_lu = 0;
-  char buffer[MAX_BUFFER_SIZE];
-  while(total_lu < file_size){
-    
-    size_t read = fread(buffer, sizeof(char), MAX_BUFFER_SIZE, file);
-    if(read == 0){
-      if(ferror(file) != 0){
-	perror("Client : erreur lors de la lecture du fichier \n");
-	fclose(file);
-	exit(1);
-      }else{
-        printf("Client : arrivé a la fin du la lecture du fichier\n");// fin du fichier
-	break;
-      }
+    if (res == ERROR || res == 0) {
+        return res;
     }
-    printf("Client : j'ai lu un bloc du fichier \n");  
-    total_lu += read;
-  }
 
-  // fermeture du fichier
-  fclose(file); 
-  close(ds);
-   
-  printf("Client : j'ai lu au total : %d octets \n", total_lu);  
- 
- 
-  printf("Client : c'est fini\n");
+    size_t totalSent = 0;
+
+    while (totalSent < messageSize) {
+        // On veut recevoir les messageSize - totalReceived octets du message.
+        // Ainsi, on ajoute les octets reçus au pointeur message + totalReceived.
+        ssize_t res = send(connectedSocket, message + totalSent, messageSize - totalSent, 0);
+
+        if (res == ERROR || res == 0) {
+            return res;
+        }
+
+        totalSent += res;
+    }
+
+    return totalSent;
+}
+
+/**
+ * Lit un fichier sur le nombre d'octets demandés.
+ * Termine le programme avec un code d'erreur si le fichier n'a pas pu être correctement lu.
+ * Retourne 0 s'il n'y a plus rien à lire dans le fichier, sinon, retourne le nombre d'octets lus.
+ */
+size_t readFile(FILE *file, char *buffer, size_t readSize) {
+    size_t read = fread(buffer, sizeof(char), readSize, file);
+    if (read == 0) {
+        if (ferror(file) != 0){
+            perror("[CLIENT] Erreur lors de la lecture du fichier ");
+            fclose(file);
+            exit(EXIT_FAILURE);
+        } 
+        else {
+            printf("[CLIENT] Fichier totalement lu.\n");
+            return 0;
+        }
+    }
+    return read;
+}
+
+int main(int argc, char *argv[]) {
+    // Un client se connecte automatiquement a un port.
+    // Il se connecte à un serveur et lui envoie le fichier désigné par nom_fichier.
+    if (argc != 4) {
+        printf("[CLIENT] Utilisation : %s ip_serveur port_serveur nom_fichier\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    // Création de la socket client qui va se connecter avec le serveur.
+    int clientSocket = createTCPSocket();
+    
+    // Connexion avec la socket du serveur, désignée par l'ip et le port donné en paramètre du programme.
+    struct sockaddr_in serverAddress = createIPv4Address(argv[1], (short)atoi(argv[2]));
+    connectTo(clientSocket, &serverAddress);
+
+    printf("[CLIENT] Connecté au serveur %s:%i.\n", 
+        inet_ntoa(serverAddress.sin_addr), ntohs(serverAddress.sin_port));
+    
+    // Envoie du fichier désigné par nom_fichier
+
+    // 1 - Envoie du nom du fichier
+    sendTCP(clientSocket, argv[3], strlen(argv[3]) + 1);
+
+    // 2 - Envoie du contenu du fichier
+    //  a) Construction du chemin du fichier
+    char *filepath = malloc(strlen(argv[3]) + 12); // ./emission/+nom fichier +\0
+    filepath[0] = '\0';
+    strcat(filepath, "./emission/");
+    strcat(filepath, argv[3]);
+
+    //  b) Lecture du fichier & envoie du contenu.
+    printf("[CLIENT] Envoie du fichier %s\n", filepath);
+
+    // Récupération de la taille du fichier à envoyer.
+    struct stat attributes;
+    if (stat(filepath, &attributes) == ERROR) {
+        perror("[CLIENT] Erreur lors de la lecture de la taille du fichier ");
+        free(filepath);
+        exit(EXIT_FAILURE);
+    }
+    size_t fileSize = attributes.st_size;
+    printf("[CLIENT] Taille du fichier (en octets) : %zu\n", fileSize);
+
+    // Envoie de la taille du fichier
+    ssize_t res = send(clientSocket, &fileSize, sizeof(fileSize), 0);
+    if (res == ERROR) {
+        perror("[CLIENT] Erreur lors de l'envoie de la taille du fichier ");
+        exit(EXIT_FAILURE); 
+    }
+    if (res == 0) {
+        printf("[CLIENT] Erreur lors de l'envoie du fichier : la socket est fermée.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lecture du contenu du fichier
+    FILE* file = fopen(filepath, "rb");
+    if (file == NULL){
+        perror("[CLIENT] Erreur lors de l'ouverture du fichier ");
+        free(filepath);
+        exit(EXIT_FAILURE);
+    }
+    free(filepath);
+
+    // On lit le fichier par bloc de MAX_BUFFER_SIZE et on l'envoie au serveur.
+    size_t totalRead = 0;
+    while (totalRead < fileSize) {
+        // Lecture du fichier
+        char buffer[MAX_BUFFER_SIZE];
+        size_t read = readFile(file, buffer, MAX_BUFFER_SIZE);
+        if (read == 0) {
+            break;
+        }
+        totalRead += read;
+
+        // Envoie au serveur distant
+        res = sendTCP(clientSocket, buffer, read);
+
+        if (res == ERROR) {
+            perror("[CLIENT] Erreur lors de l'envoie du fichier ");
+            break;
+        }
+        if (res == 0) {
+            printf("[CLIENT] Socket serveur fermée.\n");
+            break;
+        }
+
+        // Si tout s'est bien passé, on affiche le nombre d'octets actuellement envoyés au serveur.
+        printf("[CLIENT] %zu/%zu octets envoyés\n", totalRead, fileSize);
+    }
+
+    // Fermeture du fichier et de la socket
+    fclose(file); close(clientSocket);
+    
+    printf("[CLIENT] Fin de service.\n");
 }
