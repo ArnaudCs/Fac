5def554a4cdd3ebb93075ab1c87b053be1f57c4d
arnaud <arnaud.cossu@gmail.com>
2022-02-19 17:16:28 +0100

    ajout cc

--- S2/Programmation MultiTâches/Entraînement des forks/exemple-fork-pere-fils.c
+++ S2/Programmation MultiTâches/Entraînement des forks/exemple-fork-pere-fils.c
@@ -8,8 +8,8 @@
 
 int  main (int argc, char *argv[]){
 
-  if (argc != 2) {
-    printf ("Utilisation : %s nombre_de_processus_fils\n", argv[0]);
+  if (argc != 4) {
+    printf ("Utilisation : %s nombre_de_processus_fils profondeur, largeur\n", argv[0]);
     exit(1);
   }
 
@@ -18,24 +18,28 @@ int  main (int argc, char *argv[]){
   int parent = getpid();
   printf("Avant fork : pid parent racine : %d \n", parent);
 
-  int resFork = fork();
-  if (resFork == -1) {
-	perror("Erreur fork : "); 
-	exit(1);
-  }
   if (parent == getpid()){ // je suis dans le processus racine
-     printf("Après fork : je suis le processus : %d et je commence mon travail\n", parent);
-     // je fait une simulation d'un calcul en parralèle avec le fils
-     sleep (5); // s'endormir pendant 5 secondes     	
+	     printf("Après fork : je suis le processus : %d et je commence mon travail\n", parent);
+	     // je fait une simulation d'un calcul en parralèle avec le fils
+	     sleep (5); // s'endormir pendant 5 secondes     	
   }
   
-  if (parent != getpid()){
-    printf("Après fork : je suis le processus : %d et je commence mon travail \n", getpid());
-    sleep(10);
-    printf("Je suis le processus : %d et j'ai terminé mon travail\n", getpid());
-   exit(0);
+  for(int i = 0; i<(argv[2]-1); i++){
+          if(parent == getpid()){
+		  int resFork = fork();
+		  if (resFork == -1) {
+			perror("Erreur fork : "); 
+			exit(1);
+		  }
+		 
+		  if (parent != getpid()){
+		    printf("Après fork : je suis le processus : %d et je commence mon travail \n", getpid());
+		    sleep(10);
+		    printf("Je suis le processus : %d et j'ai terminé mon travail\n", getpid());
+		   exit(0);
+		  }
+	  }
   }
-  
   // la suite ici n'est faite que par le processus racince
     printf("Je suis le processus : %d. J'ai terminé mon travail et doit attendre la fin du processus fils que j'ai créé\n", getpid());  
    
