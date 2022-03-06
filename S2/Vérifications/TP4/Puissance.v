c30c9a814cb33905dd4f2ee1a1b4f8c8a9524a84
Arnaud <arnaud.cossu@gmail.com>
2022-03-06 19:25:21 +0100

    entrainement

--- S2/Vérifications/TP4/Puissance.v
+++ S2/Vérifications/TP4/Puissance.v
@@ -2,7 +2,7 @@ Require Import FunInd.
 
 Inductive is_power : nat -> nat -> nat -> Prop :=
  | is_power_0 : forall n : nat, (is_power n 0 1)
- | is_power_S : forall n f t : nat, (is_power n (S f) t) -> (is_power n f (t * n)).
+ | is_power_S : forall n f t : nat, (is_power n f t) -> (is_power n (S f) (t * n)).
  
 Fixpoint power (n f : nat) : nat :=
  match f with 
@@ -15,15 +15,16 @@ Functional Scheme power_ind := Induction for power Sort Prop.
 
 Lemma powercorrec : forall n f t : nat, (power n f) = t -> (is_power n f t).
 Proof.
- intros.
+ intro.
+ intro.
  functional induction (power n f) using power_ind.
+ intros.
  rewrite <- H.
  apply is_power_0.
  intros.
- simpl. 
  rewrite <- H.
  apply is_power_S.
- intros.
- simpl.
-
+ apply IHn0.
+ reflexivity.
 Qed.
+
